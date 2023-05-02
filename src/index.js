14e40fab0c5ebfcbcc3b1a6ddf116728db817ad8
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-05-02 12:39:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const reader = async (fileName) => {
+await fs.readFile(fileName);
   // write code here
 	// dont change function name
   
